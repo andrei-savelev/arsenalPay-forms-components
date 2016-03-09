e9d031075322a723c1b3ed12959cdb913f9e3693
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-09 18:49:54 +1000

    рефакторинг вспомогательных методов

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,4 +1,36 @@
 var _ = require('lodash');
+var _cardsSetings = [{
+    type: 'visaelectron',
+    patterns: [4026, 417500, 4405, 4508, 4844, 4913, 4917],
+    format: /(\d{1,4})/g,
+    cardLength: [16],
+    cvcLength: [3],
+    luhn: true
+},
+{
+    type: 'maestro',
+    patterns: [50, 56, 57, 58, 59, 60, 61, 62, 63, 66, 67, 68, 69],
+    format: /(\d{1,4})/g,
+    cardLength: [12, 19],
+    cvcLength: [3],
+    luhn: true
+},
+{
+    type: 'visa',
+    patterns: [4],
+    format: /(\d{1,4})/g,
+    cardLength: [13, 16],
+    cvcLength: [3],
+    luhn: true
+},
+{
+    type: 'mastercard',
+    patterns: [51, 52, 53, 54, 55],
+    format: /(\d{1,4})/g,
+    cardLength: [16],
+    cvcLength: [3],
+    luhn: true
+}];
 
 /**
  * Метод показывает тултип под полем с ошибкой или вспомогательной информацией,
@@ -45,9 +77,7 @@ function _getOnlyNumbers(value) {
 }
 
 /**
- * Метод для обработки события ввода/вставки в текстовое поле.
- * Возвращает строку в верхнем регистре
- * @param target
+ * Метод для обработки события ввода/_cardsSetingsки в текстовое поле_cardsSetings@param target
  * @returns {string}
  * @private
  */
@@ -79,70 +109,99 @@ function _hideInfo($element) {
     $element.removeClass('_show')
 }
 
-module.exports = {
+
+function _cardFromNumber (num) {
+    var card,
+        i,
+        j,
+        len1,
+        len2,
+        p,
+        pattern,
+        ref;
+
+    num = (num + '').replace(/\D/g, '');
+
+    for (i = 0, len1 = _cardsSetings.length; i < len1; i++) {
+        card = _cardsSetings[i];
+        ref = card.patterns;
+
+        for (j = 0, len2 = ref.length; j < len2; j++) {
+            pattern = ref[j];
+            p = pattern + '';
+
+            if (num.substr(0, p.length) === p) {
+                return card;
+            }
+        }
+    }
+}
+
+function _luhnCheck (num) {
+    var digit,
+        digits,
+        i,
+        len1,
+        odd,
+        sum;
+
+    odd = true;
+    sum = 0;
+    digits = (num + '').split('').reverse();
+
+    for (i = 0, len1 = digits.length; i < len1; i++) {
+        digit = digits[i];
+        digit = parseInt(digit, 10);
+
+        if ((odd = !odd)) {
+            digit *= 2;
+        }
+
+        if (digit > 9) {
+            digit -= 9;
+        }
+
+        sum += digit;
+    }
+
+    return sum % 10 === 0;
+}
+
+var utils = {
     loadFont: _loadGFont,
     showInfoTooltip: _showInfo,
+    hideInfoTooltip: _hideInfo,
     upperCase: _upperCase,
     getOnlyNumbers: _getOnlyNumbers,
+    getCard: _cardFromNumber,
+    luhnCheck: _luhnCheck,
 
-    phoneMaskOptions: {
-        onInvalid: function () {
-            _showInfo($('.js-error-tooltip'), 'error', 'Не правильно указан номер')
-        },
-
-        onComplete: function () {
-            _hideInfo($('.js-error-tooltip'))
-        }
-    },
-
-    amountMaskOptions: {
-        onKeyPress: function(cep, e, field, options){
-            var masks = ['00000', '0 0000', '00 000'];
-            var numericValue = _getOnlyNumbers(cep);
-            var trueValueLength = null;
-            var mask = null;
-
-            !_.isEmpty(cep) && (trueValueLength = _getOnlyNumbers(cep).length);
-
-            /*if ( String(numericValue).charAt(0) == 0 ) {
-                showInfo($($amountField), $($infoBLock), 'error', errorTexts.inCorrectInput);
-            } else if ( numericValue > 60 && numericValue < 75000 ) {
-                hideInfo($($amountField), $($infoBLock))
-            }*/
-
-            if (trueValueLength <= 3) {
-                mask = masks[0];
-            } else if (trueValueLength == 4) {
-                mask = masks[1];
-            } else if (trueValueLength == 5) {
-                mask = masks[2];
-            }
-
-            $(field).mask(mask, options);
-        }
-
-        /*onInvalid: function () {
-            _showInfo($($amountField), $($infoBLock), 'error', errorTexts.onlyNumbersAccepted);
-        },
-
-        onComplete: function () {
-            if ( getOnlyNumbers($($amountField).val()) > 75000 ) {
-                showInfo($($amountField), $($infoBLock), 'error', errorTexts.maxAmount);
-            } else {
-                hideInfo($($amountField), $($infoBLock))
-            }
-        }*/
-    },
-
+    /**
+     * Список текстов для поля подсказки
+     */
     messageTexts: {
         onlyNumbersAccepted: 'Допускается ввод только цифр от 0 - 9',
-        availableOperators: 'Оплата с баланса мобильного телефона доступна для абонентов ....',
         typeAmount: 'Введите сумму оплаты',
-        inCorrectInput: 'Поле заполнено неверно',
         maxAmount: 'Максимальная сумма возможного платежа 75000 руб',
         minAmount: 'Минимальная сумма для оплаты 60 руб',
-        typePhone: 'Введите номер телефона',
-        phoneFormat: 'Номер телефона должен соответствовать формату: +7 (___) ___ __ __',
-        helpPrefix: 'Введите'
-    }
+        emptyPhone: 'Введите номер телефона',
+        minPhoneLength: 'Номер введен не полностью, попробуйте еще раз',
+        invalidFirstSymbol: 'Номер телефона должен начинаться с +79',
+        checkOperators: 'Платежи доступны только для абонентов....',
+        availableOperators: 'Оплата с баланса мобильного телефона доступна для абонентов ....',
+        emptyCard: 'Введите номер карты',
+        luhnFailed: 'Номер карты введен неверно, попробуйте еще раз'
+    },
+
+    /**
+     * Список допустимых начальных первых цифр в номере карты
+     */
+    cardFirstLetters: ['4', '5', '6'],
+
+    /**
+     * Дефолтные настройки для определения типа карты
+     */
+    cardsSettings: _cardsSetings
 };
+
+module.exports = utils;
