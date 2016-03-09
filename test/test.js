2a44916ec122732c072895477e2f5bf38f1797e2
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-09 18:49:26 +1000

    написаны тесты на методы для рабты с номером карты

--- test/test.js
+++ test/test.js
@@ -4,20 +4,20 @@ var should = require('chai').should;
 var utils = require('../src/js/utils/utils');
 
 describe('UTILS >', function(){
-    describe('getOnlyNumbers', function(){
+    describe('getOnlyNumbers > ', function(){
         it('method exist', function () {
-            assert(utils.getOnlyNumbers);
+            assert( utils.getOnlyNumbers );
         });
 
         it('should return only numbers', function () {
-            var numbers = utils.getOnlyNumbers('some123');
-            assert.equal(numbers, 123);
+            var numbers = utils.getOnlyNumbers( 'some123' );
+            assert.equal( numbers, 123 );
         });
     });
 
-    describe('upperCase', function () {
+    describe('upperCase > ', function () {
         it('method exist', function () {
-            assert(utils.upperCase);
+            assert( utils.upperCase );
         });
 
         it('should return a upperCase', function () {
@@ -25,7 +25,31 @@ describe('UTILS >', function(){
                 value: 'test'
             };
 
-            expect(utils.upperCase(target)).to.equal('TEST')
+            expect( utils.upperCase(target) ).to.equal( 'TEST' )
+        })
+    });
+
+    describe('cardFromNumber > ', function () {
+        it('method exist', function () {
+            assert( utils.getCard )
+        });
+
+        it('should return mastercard', function () {
+            expect( utils.getCard( 5444870724493746 ).type ).to.equal( 'mastercard' );
+        });
+
+        it('should be maestro', function () {
+            expect( utils.getCard( 6777060877427221 ).type).to.equal( 'maestro' );
+        });
+    });
+
+    describe('luhnCheck > ', function () {
+        it('method exist', function () {
+            assert(utils.luhnCheck);
+        });
+
+        it('luhn test should be passed', function () {
+            expect( utils.luhnCheck( 6777060877427221 ) ).to.equal(true);
         })
     });
 });
