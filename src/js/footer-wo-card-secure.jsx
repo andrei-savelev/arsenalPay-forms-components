08f4260c1c92a7ef0e101d64b286baa87f581ac3
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-17 14:44:21 +1000

    очень много фиксов

---
+++ src/js/footer-wo-card-secure.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FooterWOCardSecure from './components/footer-wo-card-secure.jsx';
+
+ReactDOM.render(<FooterWOCardSecure />, document.getElementById('js-react-result-footer'));
