4727782bb2644808deaf2579582f3c03fcefd1f7
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-19 15:10:30 +1000

    добавлен вызов кастомного логгера ошибок

--- src/js/phone-form.jsx
+++ src/js/phone-form.jsx
@@ -8,6 +8,10 @@ import AccountInput from './components/account-input.jsx';
 import AmountInput from './components/amount-input.jsx';
 import SubmitButton from './components/submit-button.jsx';
 import FooterWOCardSecure from './components/footer-wo-card-secure.jsx';
+import errorLogger from '../../bower_components/error-logger/error-logger'
+
+// Отправляем ошибки на сервер для логирования
+errorLogger('http://arsenalpay.ru/p2p/log.php');
 
 let Phone = React.createClass({
     render() {
