06f95f297ac1a378f4d52b0637764adcd84ed49e
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-28 14:24:57 +1000

    imoort all forms in one file

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Phone from './phone-form.jsx';
+import Wallets from './wallet-form.jsx';
+import Card from './card-form.jsx';
 import {loadFont} from './utils/utils';
 
 loadFont();
 
-ReactDOM.render(<Phone />, document.getElementById('mk'));
+ReactDOM.render(<Wallets getDataUrl="data.json"/>, document.getElementById('wallet-form'));
+ReactDOM.render(<Phone getDataUrl="data.json" />, document.getElementById('phone-form'));
+ReactDOM.render(<Card getDataUrl="data.json" />, document.getElementById('card-form'));
+
