c6814f834df6de13c59d2d412f032bfb5d510298
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-28 13:07:45 +1000

    added default state

--- src/js/components/expire-row.jsx
+++ src/js/components/expire-row.jsx
@@ -5,14 +5,35 @@ import CVC from './cvc.jsx';
 import InfoTooltip from './info-tooltip.jsx';
 
 let ExpRow = React.createClass({
+    getInitialState() {
+        return {
+            state: 'empty',
+            cvcState: 'empty',
+            value: '',
+            cvcValue: ''
+        }
+    },
+
+    componentWillMount() {
+        this.props.attachToForm(this);
+    },
+
+    componentWillUnmount() {
+        this.props.detachFromForm(this);
+    },
+
     render() {
         return (
-            <div className="field-row">
+            <div className="field-row" name={this.props.name}>
                 <ExpSelects />
                 <CVC />
                 <InfoTooltip />
             </div>
         );
+    },
+
+    _validate() {
+        console.log('Я без валидации временно');
     }
 });
 
