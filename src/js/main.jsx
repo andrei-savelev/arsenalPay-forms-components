0fea84bf4902e3b0ac0658fc9fa19fdbcd7f2608
Andrey Savelyev <andrey.savelyev@arsenalmedia.ru>
2016-03-29 16:51:08 +1000

    Зодан прототип виджета

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -7,7 +7,103 @@ import {loadFont} from './utils/utils';
 
 loadFont();
 
-ReactDOM.render(<Wallets getDataUrl="data.json"/>, document.getElementById('wallet-form'));
-ReactDOM.render(<Phone getDataUrl="data.json" />, document.getElementById('phone-form'));
-ReactDOM.render(<Card getDataUrl="data.json" />, document.getElementById('card-form'));
+var tabList = [
+    { 'id': 1, 'name': 'Карты', 'url': '/mike' },
+    { 'id': 2, 'name': 'Телефон', 'url': '/donnie' },
+    { 'id': 3, 'name': 'Кошельки', 'url': '/raph' }
+];
 
+var Tab = React.createClass({
+    handleClick: function(e){
+        e.preventDefault();
+        this.props.handleClick();
+    },
+
+    render: function(){
+        return (
+            <li className={this.props.isCurrent ? 'widget-tabs__item active' : 'widget-tabs__item'}>
+                <a onClick={this.handleClick} href={this.props.url}>
+                    {this.props.name}
+                </a>
+            </li>
+        );
+    }
+});
+
+var Tabs = React.createClass({
+    handleClick: function(tab){
+        this.props.changeTab(tab);
+    },
+
+    render: function(){
+        return (
+            <ul className="widget-tabs">
+                {this.props.tabList.map(function(tab) {
+                    return (
+                        <Tab
+                            handleClick={this.handleClick.bind(this, tab)}
+                            key={tab.id}
+                            url={tab.url}
+                            name={tab.name}
+                            isCurrent={(this.props.currentTab === tab.id)}
+                        />
+                    );
+                }.bind(this))}
+            </ul>
+        );
+    }
+});
+
+var Content = React.createClass({
+    render: function(){
+        return(
+            <div className="content">
+                {
+                    this.props.currentTab === 1 ?
+                        <Card getDataUrl="data.json" />
+                        :null
+                }
+
+                {
+                    this.props.currentTab === 2 ?
+                        <Phone getDataUrl="data.json"/>
+                        :null
+                }
+
+                {
+                    this.props.currentTab === 3 ?
+                        <Wallets getDataUrl="data.json"/>
+                        :null
+                }
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            tabList: tabList,
+            currentTab: 1
+        };
+    },
+
+    changeTab: function(tab) {
+        this.setState({ currentTab: tab.id });
+    },
+
+    render: function(){
+        return(
+            <div className="ap-ui__widget__container">
+                <Tabs
+                    currentTab={this.state.currentTab}
+                    tabList={this.state.tabList}
+                    changeTab={this.changeTab}
+                />
+                <Content currentTab={this.state.currentTab} />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('js-react-widget'));
